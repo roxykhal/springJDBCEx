9313ac8e9be7ea461841f13ec85b608f1d2c0466
Roxy Khalilifar <roxykhal@hotmail.com>
2025-02-22 13:21:03 +0000

    rowmapper to get object of student

--- src/main/java/com/khalilifar/springJDBCEx/repo/StudentRepo.java
+++ src/main/java/com/khalilifar/springJDBCEx/repo/StudentRepo.java
@@ -2,9 +2,12 @@ package com.khalilifar.springJDBCEx.repo;
 import com.khalilifar.springJDBCEx.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,8 +34,29 @@ public class StudentRepo {
 
     public List<Student> findAll() {
 
-        List<Student> students = new ArrayList<>();
-        return students;
+        String sql = "select * from student";
+
+        //RowMapper is an interface used in Spring JDBC to map rows of a ResultSet to Java objects
+
+        RowMapper<Student> mapper = new RowMapper<Student>() {
+            @Override
+            public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+                //fetching data from RS and saving in student object
+                Student student = new Student();
+                student.setRollNo(rs.getInt("rollno"));
+                student.setName((rs.getString("name")));
+                student.setMarks(rs.getInt("marks"));
+
+
+                return student;
+            }
+        };
+
+        return jdbc.query(sql, mapper);
+
+//do not need this anymore as we are going to use JDBC, and we want to fetch all data from DB
+//        List<Student> students = new ArrayList<>();
+//        return students;
 
     }
 }
